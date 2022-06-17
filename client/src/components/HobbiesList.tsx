8af05e4381f75fd8bd1cf1a887aa305b80699f1a
Stephane Gillon <stephanegillon@hotmail.com>
2022-06-17 08:21:46 +0200

    create spot

---
+++ client/src/components/HobbiesList.tsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+// let passions : {name :string; icon :string }[] = [
+//     {name: "paintball", icon: "paintball-gunzip.png"},
+//     {name: "lego", icon: "lego-head.png"},
+//     {name: "jeu vidéo", icon: "controller.png"},
+// ];
+
+let passions : string[] = [
+  "paintball", "lego", "jeu vidéo"
+]
+
+export default passions
